0c52d6ef8c62b643d81c833f89c2e30620d4c819
Chasem11 <chasemoffat11@gmail.com>
2024-01-25 19:24:18 -0500

    Adjusted Css for the App and Schedule component

--- my-vite-app/src/App.jsx
+++ my-vite-app/src/App.jsx
@@ -6,14 +6,68 @@ function App() {
   const games = [
     {
       id: 1,
+      opponent: 'Los Angeles Chargers',
+      date: '2023-09-10',
+      location: 'Sofi Stadium',
+      image: 'https://blog.ticketmaster.com/wp-content/uploads/Gillette-Stadium-2-1024x768.jpg',
+      type: 'Regular Season',
+    },
+    {
+      id: 2,
       opponent: 'New England Patriots',
+      date: '2023-09-17',
+      location: 'Gillette Stadium',
+      image: 'https://blog.ticketmaster.com/wp-content/uploads/Gillette-Stadium-2-1024x768.jpg',
+      type: 'Regular Season',
+    },
+    {
+      id: 3,
+      opponent: 'Denver Broncos',
+      date: '2023-09-24',
+      location: 'Hard Rock Stadium',
+      image: 'https://blog.ticketmaster.com/wp-content/uploads/Gillette-Stadium-2-1024x768.jpg',
+      type: 'Regular Season',
+    },
+    {
+      id: 4,
+      opponent: 'Buffalo Bills',
+      date: '2023-10-01',
+      location: 'Highmark Stadium',
+      image: 'https://blog.ticketmaster.com/wp-content/uploads/Gillette-Stadium-2-1024x768.jpg',
+      type: 'Regular Season',
+    },
+    {
+      id: 5,
+      opponent: 'New York Giants',
+      date: '2023-10-08',
+      location: 'Hard Rock Stadium',
+      image: 'https://blog.ticketmaster.com/wp-content/uploads/Gillette-Stadium-2-1024x768.jpg',
+      type: 'Regular Season',
+    },
+    {
+      id: 6,
+      opponent: 'Carolina Panthers',
+      date: '2023-10-15',
+      location: 'Hard Rock Stadium',
+      image: 'https://blog.ticketmaster.com/wp-content/uploads/Gillette-Stadium-2-1024x768.jpg',
+      type: 'Regular Season',
+    },
+    {
+      id: 7,
+      opponent: 'Philadelphia Eagles',
       date: '2023-09-21',
       location: 'Gillette Stadium',
       image: 'https://blog.ticketmaster.com/wp-content/uploads/Gillette-Stadium-2-1024x768.jpg',
-      link: 'link_to_game_details',
       type: 'Regular Season',
     },
-    // ... more games
+    {
+      id: 8,
+      opponent: 'New England Patriots',
+      date: '2023-10-29',
+      location: 'Hard Rock Stadium',
+      image: 'https://blog.ticketmaster.com/wp-content/uploads/Gillette-Stadium-2-1024x768.jpg',
+      type: 'Regular Season',
+    },
   ];
 
   return (
@@ -22,7 +76,9 @@ function App() {
         <img src="https://seeklogo.com/images/M/miami-dolphins-logo-9713AD4C69-seeklogo.com.png" alt="Miami Dolphins Logo" className="logo" />
         <h1>Miami Dolphins Schedule</h1>
       </nav>
-      <ScheduleBoard games={games} />
+      <div className="Schedule-div">
+        <ScheduleBoard games={games} />
+      </div>
     </div>
   );
 }
